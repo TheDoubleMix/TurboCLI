973f4272636544d6108ad42bc84eb528dac9ee51
TheDoubleMix <xboxj2012@gmail.com>
2025-03-03 22:22:05 +0200

    change project name to TurboCLI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="cli",
+    name="TurboCLI",
     version="0.1.0",
     packages=find_packages(),
     author="Juho Jokisalo",
